89d0fe40443d7337f376ba5df597c2b85afae4e0
Ardhanfah <ardhandikri04@gmail.com>
2024-09-29 11:08:44 +0700

    Jobsheet 1

---
+++ Praktikum Dasar Pemrograman/Minggu 1/Latihan02.java
@@ -0,0 +1,10 @@
+/** 
+* latihan02  
+*/
+
+public class Latihan02 {
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Ardhan Dikri Achmad Fahrudin");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Praktikum Dasar Pemrograman/Minggu 1/Mahasiswa02.java
@@ -0,0 +1,15 @@
+public class Mahasiswa02 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t: Ardhan Dikri Achmad Fahrudin");
+        System.out.println("NIM\t: 823638328");
+        System.out.println("Kelas\t: 1F");
+        System.out.println("No Absen: 02");
+        System.out.println("Prodi\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t: Gedangan Kabupaten Malang");
+        System.out.println("No HP\t: 02375347342");
+        System.out.println("Hobi\t: Jogging");
+    }
+}
