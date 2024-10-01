b6cbecc57ecea76646fb52b34bb1e0f2371101cf
Ardhanfah <ardhandikri04@gmail.com>
2024-10-01 18:50:54 +0700

    tahun kabisat kelipatan 100 dan 400

--- Praktikum Dasar Pemrograman/Minggu 6/Praktikum/Pemilihan2Percobaan102.java
+++ Praktikum Dasar Pemrograman/Minggu 6/Praktikum/Pemilihan2Percobaan102.java
@@ -12,9 +12,14 @@ public class Pemilihan2Percobaan102 {
 
         if((tahun%4)==0){
             if((tahun%100!=0))
-                System.out.println("Tahun Kabisat");
+                System.out.println("Tahun Kabisat");         
             else 
-                System.out.printf("Tahun %s Bukan tahun kabisat", tahun);
+                if(tahun%100==0)
+                    if(tahun%400==0)
+                        System.out.println("Tahun Kabisat");
+                        
+                    else
+                        System.out.printf("Tahun %s Bukan tahun kabisat", tahun);
         }else
             System.out.println("Bukan tahun kabisat");
 
