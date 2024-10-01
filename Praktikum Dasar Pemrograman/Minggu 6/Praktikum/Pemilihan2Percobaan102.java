3deb50b7888d46a58a4f2f1587e1067584173338
Ardhanfah <ardhandikri04@gmail.com>
2024-10-01 10:47:42 +0700

    Tambahkan ELSE: Percobaan-1 Soal no-3

---
+++ Praktikum Dasar Pemrograman/Minggu 6/Praktikum/Pemilihan2Percobaan102.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan102 {
+    public static void main(String[] args) {
+        Scanner input02 = new Scanner(System.in);
+
+        // Deklarasi
+        int tahun;
+
+        System.out.print("Masukkan tahun: ");
+        tahun = input02.nextInt();
+
+        if((tahun%4)==0){
+            if((tahun%100!=0))
+                System.out.println("Tahun Kabisat");
+            else 
+                System.out.printf("Tahun %s Bukan tahun kabisat", tahun);
+        }else
+            System.out.println("Bukan tahun kabisat");
+
+        input02.close();
+    }
+}
