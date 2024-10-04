fab80a37979348a8d5fe41604c0570ab3ca1cd5c
Ardhanfah <ardhandikri04@gmail.com>
2024-10-04 21:33:38 +0700

    update tahun kabisat

--- Praktikum Dasar Pemrograman/Minggu 6/Praktikum/Pemilihan2Percobaan102.java
+++ Praktikum Dasar Pemrograman/Minggu 6/Praktikum/Pemilihan2Percobaan102.java
@@ -7,19 +7,19 @@ public class Pemilihan2Percobaan102 {
         // Deklarasi
         int tahun;
 
+        // Input
         System.out.print("Masukkan tahun: ");
         tahun = input02.nextInt();
 
+        // Decision IF bertingkat
         if((tahun%4)==0){
             if((tahun%100!=0))
                 System.out.println("Tahun Kabisat");         
             else 
-                if(tahun%100==0)
-                    if(tahun%400==0)
-                        System.out.println("Tahun Kabisat");
-                        
-                    else
-                        System.out.printf("Tahun %s Bukan tahun kabisat", tahun);
+                if(tahun%400==0)
+                    System.out.println("Tahun Kabisat");   
+                else
+                    System.out.printf("Tahun %s Bukan tahun kabisat", tahun);
         }else
             System.out.println("Bukan tahun kabisat");
 
