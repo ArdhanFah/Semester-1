e815089e192406e774000fa988ab5c0bb7583dbd
Ardhanfah <ardhandikri04@gmail.com>
2024-11-03 21:58:32 +0700

    Tugas Array 1 - Soal Nomor 1

---
+++ Praktikum Dasar Pemrograman/Minggu 10/Tugas/Nilai02.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Nilai02 {
+    public static void main(String[] args) {
+        Scanner input02 = new Scanner(System.in);
+
+        int jumlahMhs, jumlahNilai = 0, rata, nilMax=0, nilMin=0;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jumlahMhs = input02.nextInt();
+
+        int[] nilai = new int[jumlahMhs];
+
+        for(int i = 0; i < jumlahMhs; i++){
+            System.out.printf("Masukkan nilai mahasiswa ke-%s: ", (i+1));
+            nilai[i] = input02.nextInt();
+            jumlahNilai += nilai[i];
+            
+            nilMax = nilai[0];
+            nilMin = nilai[0];
+            
+            if(nilai[i] < nilMin){
+                nilMin = nilai[i];
+            }
+            if(nilai[i] > nilMax){
+                nilMax = nilai[i];
+            }
+        }
+
+        rata = jumlahNilai / jumlahMhs;
+        System.out.println("-------------------------------------------");
+        System.out.println("\t\tNilai Mahasiswa");
+        System.out.println("-------------------------------------------");
+        for(int i = 0; i < jumlahMhs; i++){
+            System.out.printf("\sNilai mahasiswa ke-%s: %s\n", (i+1), nilai[i]);
+        }
+        System.out.println("---------------------------------------");
+        System.out.println("rata-rata nilai mahasiswa: " + rata);
+        System.out.printf("Nilai tertinggi dari %s mahasiswa: %s\n", jumlahMhs, nilMax);
+        System.out.printf("Nilai terendah dari %s mahasiswa: %s\n", jumlahMhs, nilMin);
+
+        input02.close();
+    }
+}
